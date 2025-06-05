20f32a6a1dc17c1c98328b663eaee7b1c12d47f0
chandralegend <irugalbandarachandra@gmail.com>
2025-06-05 10:53:59 +0530

    fix: Add newline at end of vercel.json file and clean up test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import pytest
 from enum import Enum
 from nomos.utils.utils import create_base_model, create_enum
 
+
 def test_create_base_model_no_description():
     Model = create_base_model("TestModel", {"a": {"type": int, "description": ""}})
     assert Model.model_fields["a"].description is None
