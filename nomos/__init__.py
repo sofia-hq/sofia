df338cb382d70b8c02a5cea3f8f9237e5499920e
chandralegend <irugalbandarachandra@gmail.com>
2025-06-17 01:17:20 +0530

    fix: update version to 0.2.3 in __init__.py and pyproject.toml

--- nomos/__init__.py
+++ nomos/__init__.py
@@ -12,7 +12,7 @@ from .server import run_server
 from .testing import smart_assert
 from .testing.e2e import Scenario, ScenarioRunner
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "DoWhile"
 
 __all__ = [
