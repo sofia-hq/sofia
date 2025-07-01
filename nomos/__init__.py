03024cdfd6ecb77b45b5c76fbace0ef25fbdf49c
chandralegend <irugalbandarachandra@gmail.com>
2025-07-01 23:13:11 +0530

    Bump version to 0.2.5 and update release notes in index.html

--- nomos/__init__.py
+++ nomos/__init__.py
@@ -13,7 +13,7 @@ from .state_machine import StateMachine
 from .testing import smart_assert
 from .testing.e2e import Scenario, ScenarioRunner
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __author__ = "DoWhile"
 
 __all__ = [
